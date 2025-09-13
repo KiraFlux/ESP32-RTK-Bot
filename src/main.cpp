4b09d426c009ddfea84d7374d886dc12e09fe3db
KiraFlux <pret2titio1n@gmail.com>
2025-09-13 13:43:33 +0300

    init

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+#include "tools/Logger.hpp"
+
+void setup() {
+
+  Serial.begin(115200);
+
+  Logger::instance().write_func = [](const char *text, size_t lenght) {
+    Serial.write(text, lenght);
+  };
+
+  Logger_info("");
+}
+
+void loop() {
+}
