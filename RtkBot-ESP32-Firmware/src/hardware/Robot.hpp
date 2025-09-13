c036f9eaf5a80c66de62a7d52623596adfe61d5b
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-09-13 22:49:24 +0300

    ref

--- RtkBot-ESP32-Firmware/src/hardware/Robot.hpp
+++ RtkBot-ESP32-Firmware/src/hardware/Robot.hpp
@@ -3,7 +3,52 @@
 #include "tools/Singleton.hpp"
 
 #include "hardware/Motor.hpp"
+#include "kf/Storage.hpp"
 
+
+/// @brief Аппаратное обеспечение робота
 struct Robot : Singleton<Robot> {
-  friend struct Singleton<Robot>;
-}
+    friend struct Singleton<Robot>;
+
+    /// Настройки аппаратного обеспечения
+    struct Settings {
+
+        /// Параметры левого мотора
+        Motor::Settings left{
+            .max_pwm=255,
+            .direction=Motor::Settings::Direction::CCW
+        };
+
+        /// Параметры правого мотора
+        Motor::Settings right{
+            .max_pwm=255,
+            .direction=Motor::Settings::Direction::CW
+        };
+
+        /// Получить ссылку на Singleton экземпляр хранилища настроек
+        static kf::Storage<Settings> &storage() {
+            static kf::Storage<Settings> instance{
+                .key="r-set",
+                .settings = {}
+            };
+            return instance;
+        }
+
+        Settings(const Settings &) = delete;
+
+    private:
+        Settings() = default;
+    };
+
+    /// Левый мотор
+    Motor left_motor{Settings::storage().settings.left, GPIO_NUM_27, GPIO_NUM_21};
+
+    /// Правый мотор
+    Motor right_motor{Settings::storage().settings.right, GPIO_NUM_19, GPIO_NUM_18};
+
+    /// Инициализировать всю периферию
+    void init() const {
+        left_motor.init();
+        right_motor.init();
+    }
+};
--- RtkBot-ESP32-Firmware/src/main.cpp
+++ RtkBot-ESP32-Firmware/src/main.cpp
@@ -1,29 +1,9 @@
 #include <Arduino.h>
 
-#include "hardware/Motor.hpp"
+#include "hardware/Robot.hpp"
+
 #include "kf/Logger.hpp"
-#include "kf/Storage.hpp"
 
-
-static kf::Storage<Motor::Settings> left_motor_storage{
-    .key = "lms",
-    .settings = {.max_pwm = 255, .direction = Motor::Settings::Direction::CCW}
-};
-
-static kf::Storage<Motor::Settings> right_motor_storage{
-    .key = "rms",
-    .settings = {.max_pwm = 255, .direction = Motor::Settings::Direction::CW}
-};
-
-static const Motor left_motor{
-    left_motor_storage.settings, GPIO_NUM_27,
-    GPIO_NUM_21
-};
-
-static const Motor right_motor{
-    right_motor_storage.settings, GPIO_NUM_19,
-    GPIO_NUM_18
-};
 
 void setup() {
     Serial.begin(115200);
@@ -32,11 +12,8 @@ void setup() {
 
     kf::Logger::instance().write_func = [](const char *text, size_t length) { Serial.write(text, length); };
 
-    left_motor.init();
-    right_motor.init();
-
-    left_motor_storage.load();
-    right_motor_storage.load();
+    Robot::instance().init();
+    Robot::Settings::storage().load();
 
     kf_Logger_debug("init ok");
 }
