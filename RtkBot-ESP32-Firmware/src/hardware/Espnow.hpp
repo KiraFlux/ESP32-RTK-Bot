4cbc36e2d309f4d289eb7806f2d64ba152419570
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-09-13 23:46:06 +0300

    Espnow ok

---
+++ RtkBot-ESP32-Firmware/src/hardware/Espnow.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <WiFi.h>
+#include "espnow/Mac.hpp"
+
+#include "kf/Logger.hpp"
+#include "espnow/Protocol.hpp"
+
+
+/// @brief Узел Espnow
+struct Espnow {
+
+public:
+
+    /// Настройки узла
+    struct Settings {
+        /// @brief MAC-Адрес пульта
+        espnow::Mac remote_controller_mac;
+    };
+
+private:
+
+    /// @brief Настройки узла
+    const Settings &settings;
+
+public:
+
+    /// @brief Обработчик входящего пакета от пульта
+    std::function<void(const void *, rs::u8)> on_receive{nullptr};
+
+    explicit Espnow(const Settings &settings) :
+        settings{settings} {}
+
+    /// @brief Инициализировать протокол
+    /// @returns <code>true</code> - Успешная инициализация
+    bool init() const {
+        kf_Logger_info("init");
+
+        const bool wifi_ok = WiFiClass::mode(WIFI_MODE_STA);
+        if (not wifi_ok) {
+            return false;
+        }
+
+        const auto init_result = espnow::Protocol::init();
+        if (init_result.fail()) {
+            kf_Logger_error(rs::toString(init_result.error));
+            return false;
+        }
+
+        const auto peer_result = espnow::Peer::add(settings.remote_controller_mac);
+        if (peer_result.fail()) {
+            kf_Logger_error(rs::toString(peer_result.error));
+            return false;
+        }
+
+        auto receive_handler = [this](const espnow::Mac &mac, const void *data, rs::u8 len) {
+            if (this->on_receive and mac == this->settings.remote_controller_mac) { this->on_receive(data, len); }
+        };
+        const auto handler_result = espnow::Protocol::instance().setReceiveHandler(receive_handler);
+        if (handler_result.fail()) {
+            kf_Logger_error(rs::toString(handler_result.error));
+            return false;
+        }
+
+        kf_Logger_debug("success");
+        return true;
+    }
+
+    /// @brief Отправить пакет данных на пульт
+    template<typename T> inline rs::Result<void, espnow::Protocol::SendError> send(const T &value) {
+        return espnow::Protocol::send(settings.remote_controller_mac, value);
+    }
+
+    /// @brief Отправить буфер на пульт
+    inline rs::Result<void, espnow::Protocol::SendError> send(const void *data, rs::u8 size) {
+        return espnow::Protocol::send(settings.remote_controller_mac, data, size);
+    }
+};
--- RtkBot-ESP32-Firmware/src/main.cpp
+++ RtkBot-ESP32-Firmware/src/main.cpp
@@ -10,12 +10,25 @@ void setup() {
 
     kf_Logger_info("begin");
 
-    kf::Logger::instance().write_func = [](const char *text, size_t length) { Serial.write(text, length); };
+    kf::Logger::instance().write_func = [](const char *buffer, size_t size) { Serial.write(buffer, size); };
 
-    Robot::instance().init();
-    Robot::Settings::storage().load();
+    auto &robot = Robot::instance();
+    robot.init();
+
+
+    auto &robot_storage = Robot::Settings::storage();
+    if (not robot_storage.load()) {
+        robot_storage.save();
+    }
+
+    robot.esp_now.on_receive = [](const void *, rs::u8 size) {
+        kf_Logger_info("got %d bytes", size);
+    };
 
     kf_Logger_debug("init ok");
 }
 
-void loop() { delay(5000); }
+void loop() {
+    kf_Logger_debug("beep: %ld", millis());
+    delay(5000);
+}
