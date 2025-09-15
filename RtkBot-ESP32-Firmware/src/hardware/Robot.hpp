0aed99d7a796169b96e0b8780571b2f092837ea7
KiraFlux <pret2titio1n@gmail.com>
2025-09-15 17:20:41 +0300

    Энкодеры добавлены в структуру робота

--- RtkBot-ESP32-Firmware/src/hardware/Robot.hpp
+++ RtkBot-ESP32-Firmware/src/hardware/Robot.hpp
@@ -14,15 +14,14 @@ struct Robot : Singleton<Robot> {
 
     /// Настройки аппаратного обеспечения
     struct Settings {
-        
         /// Настройки ШИМ
         Motor::PwmSettings pwm;
         /// Настройки драйверов моторов
         Motor::DriverSettings left_motor, right_motor;
-        
-        /// 
-        Encoder::GenericSettings generic_encoder_settings;
-        /// @brief 
+
+        /// Общие настройки энкодеров
+        Encoder::GenericSettings generic_encoder;
+        /// Настройки подключения энкодеров
         Encoder::PinoutSettings left_encoder, right_encoder;
 
         /// Настройки узла Espnow
@@ -36,7 +35,7 @@ struct Robot : Singleton<Robot> {
             .pwm = {
                 .ledc_resolution_bits = 10,
                 .ledc_frequency_hz = 20000,
-                .dead_zone = 568,
+                .dead_zone = 568, // Значение получено эксперементально
             },
             .left_motor = {
                 .direction = Motor::DriverSettings::Direction::CCW,
@@ -50,13 +49,34 @@ struct Robot : Singleton<Robot> {
                 .speed_pin = static_cast<rs::u8>(GPIO_NUM_18),
                 .ledc_channel = 1,
             },
-            .esp_now = {.remote_controller_mac = {0x78, 0x1c, 0x3c, 0xa4, 0x96, 0xdc}}}};
+            .generic_encoder = {
+                .ticks_in_one_mm = 1.0f,
+            },
+            .left_encoder = {
+                .pin_phase_a = static_cast<rs::u8>(GPIO_NUM_NC),
+                .pin_phase_b = static_cast<rs::u8>(GPIO_NUM_NC),
+                .mode = Encoder::PinoutSettings::Mode::Rising,
+            },
+            .right_encoder = {
+                .pin_phase_a = static_cast<rs::u8>(GPIO_NUM_NC),
+                .pin_phase_b = static_cast<rs::u8>(GPIO_NUM_NC),
+                .mode = Encoder::PinoutSettings::Mode::Rising,
+            },
+            .esp_now = {
+                .remote_controller_mac = {0x78, 0x1c, 0x3c, 0xa4, 0x96, 0xdc},
+            }}};
 
     /// Левый мотор
     Motor left_motor{storage.settings.left_motor, storage.settings.pwm};
 
     /// Правый мотор
     Motor right_motor{storage.settings.right_motor, storage.settings.pwm};
+
+    /// Левый Энкодер
+    Encoder left_encoder{storage.settings.left_encoder, storage.settings.generic_encoder};
+
+    /// Правый Энкодер
+    Encoder right_encoder{storage.settings.right_encoder, storage.settings.generic_encoder};
 
     /// Узел протокола Espnow
     Espnow esp_now{storage.settings.esp_now};
@@ -71,14 +91,17 @@ struct Robot : Singleton<Robot> {
             }
         }
 
+        // Софт компоненты
+
+        if (not esp_now.init()) { return false; }
+
         // Аппаратные компоненты
 
         if (not left_motor.init()) { return false; }
         if (not right_motor.init()) { return false; }
 
-        // Софт компоненты
-
-        if (not esp_now.init()) { return false; }
+        left_encoder.init();
+        right_encoder.init();
 
         return true;
     }
