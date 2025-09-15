625bf33aef9a128ab6c4f76890dc4a3c9e019db5
KiraFlux <pret2titio1n@gmail.com>
2025-09-15 10:19:50 +0300

    Добавлены общие настройки Encoder::GenericSettings

--- RtkBot-ESP32-Firmware/src/hardware/Encoder.hpp
+++ RtkBot-ESP32-Firmware/src/hardware/Encoder.hpp
@@ -9,6 +9,18 @@ struct Encoder {
     /// Type Alias for Encoder position
     using Tick = rs::i32;
 
+    /// Type Alias for position in mm
+    using Mm = rs::f64;
+
+    /// Generic (Common) Encoder settings
+    struct GenericSettings {
+        /// Ticks to mm convertation constant
+        rs::f32 ticks_in_one_mm;
+
+        Mm toMm(Tick ticks) const { return Mm(ticks) / ticks_in_one_mm; }
+        Tick toTicks(Mm mm) const { return Tick(mm * ticks_in_one_mm); }
+    };
+
     /// Encoder pinout settings
     struct PinoutSettings {
         /// Main (Int) pin
@@ -24,11 +36,15 @@ struct Encoder {
     };
 
     const PinoutSettings &pinout_settings;
+    const GenericSettings &generic_settings;
+
+private:
     /// Current Encoder position
     Tick position{0};
 
-    explicit Encoder(const PinoutSettings &pinout_settings) :
-        pinout_settings{pinout_settings} {}
+public:
+    explicit Encoder(const PinoutSettings &pinout_settings, const GenericSettings &generic_settings) :
+        pinout_settings{pinout_settings}, generic_settings{generic_settings} {}
 
     /// Initialize Encoder pins
     void init() const {
@@ -49,6 +65,18 @@ struct Encoder {
     void disable() const {
         detachInterrupt(pinout_settings.pin_phase_a);
     }
+
+    /// Get current Encoder position in Ticks
+    inline Tick positionTicks() const { return position; }
+
+    /// Set current Encoder position in Ticks
+    void setPositionTicks(Tick new_positon) { position = new_positon; }
+
+    /// Get current Encoder position in mm
+    inline Mm positionMm() const { return generic_settings.toMm(position); }
+
+    /// Set current Encoder position in Ticks
+    void setPositionMm(Mm new_positon) { position = generic_settings.toTicks(new_positon); }
 
 private:
     /// On Main phase interrupt handler
