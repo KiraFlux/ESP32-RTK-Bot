b03d885c6c0b7474a36d97049da360615ad910de
KiraFlux <pret2titio1n@gmail.com>
2025-09-17 17:06:07 +0300

    Добавлен таймер

---
+++ RtkBot-ESP32-Firmware/src/zms/tools/Timer.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include "zms/aliases.hpp"
+
+namespace zms {
+
+/// Таймер
+struct Timer {
+
+    /// Период срабатывания таймера
+    Microseconds period;
+
+private:
+    Microseconds last{0};
+
+public:
+    explicit Timer(Milliseconds period) :
+        period{period} {}
+
+    /// Прошел ли период
+    bool alarmed() {
+        const auto now = millis();
+        const auto delta = now - last;
+
+        if (delta < period) { return false; }
+
+        last = now;
+
+        return true;
+    }
+};
+
+}// namespace zms
