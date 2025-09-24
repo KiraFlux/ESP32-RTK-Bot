aabea132cf97ecb347a723e3315684c28bb9a1cf
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-09-24 11:33:05 +0300

    Рефакторинг main.cpp Увеличение ответственности класса zms::Robot

--- Zoomers-ESP32-Firmware/src/zms/Robot.hpp
+++ Zoomers-ESP32-Firmware/src/zms/Robot.hpp
@@ -7,6 +7,13 @@
 #include "zms/drivers/Encoder.hpp"
 #include "zms/drivers/Motor.hpp"
 #include "zms/remote/Espnow.hpp"
+#include "zms/remote/EspnowRemoteController.hpp"
+#include "zms/ui/pages/EncoderConversionSettingsPage.hpp"
+#include "zms/ui/pages/EncoderTunePage.hpp"
+#include "zms/ui/pages/EspnowNodeSettingsPage.hpp"
+#include "zms/ui/pages/MainPage.hpp"
+#include "zms/ui/pages/MotorPwmSettingsPage.hpp"
+#include "zms/ui/pages/MotorTunePage.hpp"
 
 /// MISIS-Zoomers
 namespace zms {
@@ -91,6 +98,9 @@ struct Robot : Singleton<Robot> {
     /// Узел протокола Espnow
     EspnowNode espnow_node{storage.settings.espnow_node};
 
+    /// Удаленный контроллер
+    EspnowRemoteController espnow_remote_controller{};
+
     /// Инициализировать всю периферию
     bool init() {
         // Попытка загрузить настройки
@@ -101,10 +111,6 @@ struct Robot : Singleton<Robot> {
             }
         }
 
-        // Софт компоненты
-
-        if (not espnow_node.init()) { return false; }
-
         // Аппаратные компоненты
 
         if (not left_motor.init()) { return false; }
@@ -113,7 +119,166 @@ struct Robot : Singleton<Robot> {
         left_encoder.init();
         right_encoder.init();
 
+        // Софт компоненты
+
+        if (not espnow_node.init()) { return false; }
+        espnow_node.on_receive = Robot::onEspnowRemoteControllerPacket;
+
+        // TUI
+
+        setupTUI();
+
         return true;
+    }
+
+    /// Прокрутка событий робота
+    void poll() {
+        pollTUI();
+        pollRemoteControl();
+    }
+
+private:
+    void pollRemoteControl() {
+        if (espnow_remote_controller.isPacketTimeoutExpired()) {
+            if (not espnow_remote_controller.disconnected) {
+                kf_Logger_info("disconnected");
+                espnow_remote_controller.disconnected = true;
+
+                espnow_remote_controller.resetControlPacket();
+                left_motor.stop();
+                right_motor.stop();
+            }
+        } else {
+            espnow_remote_controller.disconnected = false;
+
+            float left_power, right_power;
+            espnow_remote_controller.calc(left_power, right_power);
+            left_motor.set(left_power);
+            right_motor.set(right_power);
+        }
+    }
+
+    // TUI
+
+    void setupTUI() {
+        auto &page_manager = kf::tui::PageManager::instance();
+
+        // Хранилище
+        {
+            /// Сохранить настройки
+            static kf::tui::Button save{"Save", [this](kf::tui::Button &) { storage.save(); }};
+            /// Загрузить настройки
+            static kf::tui::Button load{"Load", [this](kf::tui::Button &) { storage.load(); }};
+
+            auto &p = MainPage::instance();
+
+            p.add(save);
+            p.add(load);
+        }
+
+        // Моторы
+
+        static zms::MotorTunePage left_motor_tune_page{
+            "Motor L",
+            left_motor,
+            storage.settings.motor_pwm,
+            storage.settings.left_motor};
+
+        static zms::MotorTunePage right_motor_tune_page{
+            "Motor R",
+            right_motor,
+            storage.settings.motor_pwm,
+            storage.settings.right_motor};
+
+        static zms::MotorPwmSettingsPage motor_pwm_settings_page{
+            storage.settings.motor_pwm};
+
+        // Энкодеры
+
+        static zms::EncoderTunePage left_encoder_tune_page{
+            "Encoder L",
+            left_encoder,
+            storage.settings.left_encoder};
+
+        static zms::EncoderTunePage right_encoder_tune_page{
+            "Encoder R",
+            right_encoder,
+            storage.settings.left_encoder};
+
+        static zms::EncoderConversionSettingsPage encoder_conversion_settings_page{
+            storage.settings.encoder_conversion};
+
+        //
+
+        static zms::EspnowNodeSettingsPage espnow_node_settings_page{
+            storage.settings.espnow_node};
+
+        page_manager.bind(zms::MainPage::instance());
+
+        // Отправка актуального вида после инициализации
+        sendTUI(page_manager);
+    }
+
+    void sendTUI(kf::tui::PageManager &page_manager) {
+        const auto slice = page_manager.render();
+        const auto result = espnow_node.send(slice.data, slice.len);
+
+        kf_Logger_debug("Send: %d bytes", slice.len);
+
+        if (result.fail()) { kf_Logger_error(rs::toString(result.error)); }
+    }
+
+    void pollTUI() {
+        static auto &page_manager = kf::tui::PageManager::instance();
+
+        const bool update_required = page_manager.pollEvents();
+        if (update_required) { sendTUI(page_manager); }
+    }
+
+    // espnow
+
+    static void onEspnowRemoteControllerPacket(const void *data, rs::u8 size) {
+
+        /// Действие в меню
+        enum Action : rs::u8 {
+            None = 0x00,
+            Reload = 0x10,
+            Click = 0x20,
+            Left = 0x30,
+            Right = 0x31,
+            Up = 0x40,
+            Down = 0x41
+        };
+
+        auto translate = [](Action code) {
+            using kf::tui::Event;
+
+            switch (code) {
+                case Action::Reload: return Event::Update;
+                case Action::Click: return Event::Click;
+                case Action::Left: return Event::ChangeIncrement;
+                case Action::Right: return Event::ChangeDecrement;
+                case Action::Up: return Event::ElementPrevious;
+                case Action::Down: return Event::ElementNext;
+                case Action::None:
+                default: return Event::None;
+            }
+        };
+
+        static auto &espnow_remote_controller = Robot::instance().espnow_remote_controller;
+        static auto &page_manager = kf::tui::PageManager::instance();
+
+        switch (size) {
+            case sizeof(EspnowRemoteController::ControlPacket)://
+                espnow_remote_controller.updateControlPacket(*static_cast<const EspnowRemoteController::ControlPacket *>(data));
+                return;
+
+            case sizeof(Action)://
+                page_manager.addEvent(translate(*static_cast<const Action *>(data)));
+                return;
+
+            default: kf_Logger_warn("Unknown packet: (%d bytes)", size);
+        }
     }
 };
 
