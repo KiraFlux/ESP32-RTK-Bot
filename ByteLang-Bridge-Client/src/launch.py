7a67c1cc77208e973cc94450bff7c27d5316f67c
KiraFlux <pret2titio1n@gmail.com>
2025-09-29 21:41:55 +0300

    Генеральная очистка от нейропомоев!

--- ByteLang-Bridge-Client/src/launch.py
+++ ByteLang-Bridge-Client/src/launch.py
@@ -1,39 +1,16 @@
-from time import sleep
-from typing import Mapping
+"""
+Точка входа
+"""
 
 from robot import Robot
-
-
-def _walk_bath(robot: Robot) -> Mapping[int, int]:
-    ret = dict()
-    bars_in_bath = 10
-
-    for row in range(bars_in_bath):
-        robot.go_dist(100)
-        magnets_in_row = robot.check_magnetometer()
-
-        if magnets_in_row > 0:
-            ret[row] = magnets_in_row
-
-    return ret
 
 
 def _launch():
     robot = Robot()
     robot.start_poll_task()
-    sleep(5)
-
-    # Начало алгоритма
-
-    robot.send_millis_request(None)
-
-    sleep(1)
-
-    for i in range(5):
-        robot.go_dist(50)
-        robot.go_dist(-50)
 
     robot.poll_task.join()
 
 
-_launch()
+if __name__ == '__main__':
+    _launch()
